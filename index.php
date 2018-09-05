d8cea9b93eb67a1054d3c4ee57d319d7ec1bef00
tueseed <tue_seed@hotmail.com>
2018-09-05 10:09:50 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -11,6 +11,26 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+<style type="text/css">
+@media print{
+.print_text {
+	font-size:26px;
+	font-family:"Angsana New";
+}
+}
+#container-fluid{
+margin-bottom: 10px;
+}
+.table-responsive{
+  display: block;
+  max-height: 500px;
+  overflow-y: auto;
+  -ms-overflow-style: -ms-autohiding-scrollbar;
+}
+.form-group2{
+    text-align: center;
+}
+</style>
 </head>
 <body>
 <div class="w3-container w3-purple">
@@ -19,6 +39,51 @@
             <h1>ระบบประเมินความพึงพอใจ การไฟฟ้าส่วนภูมิภาคอำเภอโพธาราม</h1>
         </div>
     </div>
+</div>
+<div class="container-fluid">
+	<div class="row">
+        <div class="col-lg-12">
+            <div class="form-group2">
+                <h2>กรุณาประเมินความพึงพอใจ</h2>
+            </div>
+        </div>
+    </div>
+	<div class="row">
+        <div class="col-lg-12">
+            <div class="form-group">
+                    <input type="button" class="btn btn-success" value="พอใจมาก"/>
+            </div>
+        </div>
+    </div>
+	<div class="row">
+        <div class="col-lg-12">
+            <div class="form-group">
+                    <input type="button" class="btn btn-success" value="พอใจ"/>
+            </div>
+        </div>
+    </div>
+	<div class="row">
+        <div class="col-lg-12">
+            <div class="form-group">
+                    <input type="button" class="btn btn-success" value="ปานกลาง"/>
+            </div>
+        </div>
+    </div>
+	<div class="row">
+        <div class="col-lg-12">
+            <div class="form-group">
+                    <input type="button" class="btn btn-success" value="ไม่พอใจ"/>
+            </div>
+        </div>
+    </div>
+	<div class="row">
+        <div class="col-lg-12">
+            <div class="form-group">
+                    <input type="button" class="btn btn-success" value="ไม่พอใจมาก"/>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 
